607f35214ffae87c6214ddbd886bf6320d5926ae
Tomasflorio <tomasflorio00@gmail.com>
2022-09-21 17:39:06 -0300

    Creacion componente Home y Login, Arreglo error CORS

--- ProyectoFinal_TomasFlorio/Proyecto_Final_TomasFlorio_Frontend/src/app/app.module.ts
+++ ProyectoFinal_TomasFlorio/Proyecto_Final_TomasFlorio_Frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,7 +12,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component'; 
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,9 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
