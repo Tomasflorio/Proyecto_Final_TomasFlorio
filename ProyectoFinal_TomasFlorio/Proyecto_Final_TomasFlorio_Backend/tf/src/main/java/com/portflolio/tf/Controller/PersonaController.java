fa96f8366808df6ad0270a9a16801e22cd3fb7c4
Tomasflorio <tomasflorio00@gmail.com>
2022-09-25 17:25:11 -0300

    Reparación Login Jwt

--- ProyectoFinal_TomasFlorio/Proyecto_Final_TomasFlorio_Backend/tf/src/main/java/com/portflolio/tf/Controller/PersonaController.java
+++ ProyectoFinal_TomasFlorio/Proyecto_Final_TomasFlorio_Backend/tf/src/main/java/com/portflolio/tf/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portflolio.tf.Entity.Persona;
 import com.portflolio.tf.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,18 +31,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona) {
         ipersonaService.savePersona(persona);
         return "la persona fue creada correctamente";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
         ipersonaService.deletePersona(id);
         return "La persona ha sido borrada";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id, @RequestParam("nombre") String nuevoNombre, @RequestParam("apellido") String nuevoApellido, @RequestParam("img") String nuevoimg) {
         Persona persona = ipersonaService.findPersona(id);
